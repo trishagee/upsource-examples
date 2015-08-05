093cf45b0bf69a14cbd40ce31ba2e5238c65a0be
Trisha Gee <trisha.gee@gmail.com>
2015-08-05 18:37:03 +0200

    Added ability to get a list of all customers

--- src/main/java/com/mechanitis/blog/upsource/customer/Customer.java
+++ src/main/java/com/mechanitis/blog/upsource/customer/Customer.java
@@ -1,4 +1,13 @@
 package com.mechanitis.blog.upsource.customer;
 
 public class Customer {
+    private int customerId;
+    private String firstName;
+    private String lastName;
+
+    public Customer(int customerId, String firstName, String lastName) {
+        this.customerId = customerId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 }
--- src/main/java/com/mechanitis/blog/upsource/customer/CustomerDao.java
+++ src/main/java/com/mechanitis/blog/upsource/customer/CustomerDao.java
@@ -10,6 +10,26 @@ import java.util.List;
 public class CustomerDao {
     private final Database database = new Database();
 
+    public List<Customer> getAllCustomers() {
+        List<Integer> customerIds = getAllCustomerIds();
+        ArrayList<Customer> customers = new ArrayList<>();
+        try (Connection connection = database.getConnection();
+             Statement statement = connection.createStatement()) {
+            for (Integer customerId : customerIds) {
+                try (ResultSet rs = statement.executeQuery("SELECT * FROM Customers WHERE id = " + customerId)) {
+                    rs.next();
+                    customers.add(new Customer(
+                            customerId,
+                            rs.getString("first"),
+                            rs.getString("last")
+                    ));
+                }
+            }
+        } catch (SQLException e) {
+            doDatabaseErrorHandling(e);
+        }
+        return customers;
+    }
 
     public List<Integer> getAllCustomerIds() {
         List<Integer> customerIds = new ArrayList<>();
