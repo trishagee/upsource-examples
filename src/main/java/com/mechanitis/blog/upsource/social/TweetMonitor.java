ce7567cab645624ad42148c3aa06620ba2219c32
Trisha Gee <trisha.gee@gmail.com>
2015-08-05 19:05:25 +0200

    Track social media mentions by user

--- src/main/java/com/mechanitis/blog/upsource/social/TweetMonitor.java
+++ src/main/java/com/mechanitis/blog/upsource/social/TweetMonitor.java
@@ -6,6 +6,11 @@ import java.util.Map;
 public class TweetMonitor {
     private final Map<String, TwitterUser> allTwitterUsers = new HashMap<>();
 
+    public void onMessage(String twitterHandle, String tweet) {
+        TwitterUser twitterUser = allTwitterUsers.computeIfAbsent(twitterHandle, TwitterUser::new);
+        twitterUser.addMessage(tweet);
+    }
+
     public TwitterUser getTwitterUser(String twitterHandle) {
         return allTwitterUsers.get(twitterHandle);
     }
