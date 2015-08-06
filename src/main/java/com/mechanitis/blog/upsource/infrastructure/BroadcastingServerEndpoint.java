78e6e6d6e63834cb7e5ffdfa4de7fb27cd37b3b2
Trisha Gee <trisha.gee@gmail.com>
2015-08-06 12:57:09 +0200

    Using a thread safe data structure

--- src/main/java/com/mechanitis/blog/upsource/infrastructure/BroadcastingServerEndpoint.java
+++ src/main/java/com/mechanitis/blog/upsource/infrastructure/BroadcastingServerEndpoint.java
@@ -3,13 +3,13 @@ package com.mechanitis.blog.upsource.infrastructure;
 import javax.websocket.Endpoint;
 import javax.websocket.EndpointConfig;
 import javax.websocket.Session;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 import static com.mechanitis.blog.upsource.infrastructure.MessageSender.sendMessageToClient;
 
 public class BroadcastingServerEndpoint<T> extends Endpoint {
-    private final List<Session> sessions = new ArrayList<>();
+    private final List<Session> sessions = new CopyOnWriteArrayList<>();
 
     @Override
     public void onOpen(Session session, EndpointConfig config) {
