ba8f1f89a9e060e466e9b81b1385d1df40b2c418
Trisha Gee <trisha.gee@gmail.com>
2015-08-06 13:06:30 +0200

    Added order counter

--- src/main/java/com/mechanitis/blog/upsource/customer/Customer.java
+++ src/main/java/com/mechanitis/blog/upsource/customer/Customer.java
@@ -4,10 +4,15 @@ public class Customer {
     private int customerId;
     private String firstName;
     private String lastName;
+    private CustomerOrders orders;
 
     public Customer(int customerId, String firstName, String lastName) {
         this.customerId = customerId;
         this.firstName = firstName;
         this.lastName = lastName;
+    }
+
+    public void incrementOrders() {
+        orders.setNumberOfOrders(orders.getNumberOfOrders() + 1);
     }
 }
---
+++ src/main/java/com/mechanitis/blog/upsource/customer/CustomerOrders.java
@@ -0,0 +1,13 @@
+package com.mechanitis.blog.upsource.customer;
+
+public class CustomerOrders {
+    private int numberOfOrders;
+
+    public int getNumberOfOrders() {
+        return numberOfOrders;
+    }
+
+    public void setNumberOfOrders(int numberOfOrders) {
+        this.numberOfOrders = numberOfOrders;
+    }
+}
