151b225cb4188669b8831c1702232f06414eefc4
Trisha Gee <trisha.gee@gmail.com>
2015-08-28 15:18:00 +0200

    Adding gift card order

---
+++ src/main/java/com/mechanitis/blog/upsource/order/GiftCardOrder.java
@@ -0,0 +1,21 @@
+package com.mechanitis.blog.upsource.order;
+
+import com.mechanitis.blog.upsource.customer.Customer;
+
+import java.math.BigDecimal;
+
+public class GiftCardOrder implements Order {
+    private BigDecimal price;
+    private Customer customer;
+
+    public GiftCardOrder(Customer customer, BigDecimal price) {
+        this.customer = customer;
+        this.price = price;
+    }
+
+    @Override
+    public void placeOrder(Warehouse warehouse) {
+        customer.addBalanceToWallet(price);
+    }
+
+}
--- src/test/java/com/mechanitis/blog/upsource/order/OrderTest.java
+++ src/test/java/com/mechanitis/blog/upsource/order/OrderTest.java
@@ -1,7 +1,9 @@
 package com.mechanitis.blog.upsource.order;
 
+import com.mechanitis.blog.upsource.customer.Customer;
 import org.junit.Test;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import static java.util.Arrays.asList;
@@ -10,11 +12,14 @@ import static org.mockito.Mockito.verify;
 
 public class OrderTest {
     private static final int ITEM_ID = 1;
+    private static final Customer CUSTOMER = new Customer(2, "first", "last");
+    private static final BigDecimal PRICE = new BigDecimal(10);
 
     @Test
     public void shouldRemoveItemFromInventory() {
         List<Order> allOrderTypes = asList(new BookOrder(ITEM_ID),
-                                           new ElectronicsOrder(ITEM_ID));
+                                           new ElectronicsOrder(ITEM_ID),
+                                           new GiftCardOrder(CUSTOMER, PRICE));
 
         for (Order order : allOrderTypes) {
             Warehouse warehouse = mock(Warehouse.class);
