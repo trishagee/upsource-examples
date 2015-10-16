68aae083c53c8b9fd3992a8c7860c682d5964eb4
Trisha Gee <trisha.gee@gmail.com>
2015-10-16 17:33:40 +0200

    Suppress warnings

--- src/main/java/com/mechanitis/blog/upsource/customer/CustomerOrders.java
+++ src/main/java/com/mechanitis/blog/upsource/customer/CustomerOrders.java
@@ -1,5 +1,6 @@
 package com.mechanitis.blog.upsource.customer;
 
+@SuppressWarnings("unused") //this is phase 1 of development. Code should be used when ISSUE-2574 is done.
 public class CustomerOrders {
     private int numberOfOrders;
 
